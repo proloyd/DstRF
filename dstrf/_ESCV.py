e793c73a76232deaee176e0b0660e1c6ea2a94a0
proloyd <proloy@umd.edu>
2018-09-15 02:20:06 -0400

    multiple-covariates

--- dstrf/_ESCV.py
+++ dstrf/_ESCV.py
@@ -6,7 +6,8 @@ from math import sqrt
 
 
 class DstRFcv(DstRF):
-    def __init__(self, lead_field, noise_covariance, n_trials, filter_length=200, n_iter=50, n_iterc=5, n_iterf=20, ES=True):
+    def __init__( self, lead_field, noise_covariance, n_trials, filter_length=200, n_iter=50, n_iterc=5, n_iterf=20,
+            ES=True ):
         # DstRF.__init__(self, lead_field, noise_covariance, n_trials, filter_length=filter_length,
         #                n_iter=n_iter, n_iterf=n_iterf)
         DstRF.__init__(self, lead_field, noise_covariance, n_trials, filter_length=filter_length,
@@ -19,25 +20,42 @@ class DstRFcv(DstRF):
     def setup_cv(self, meg, stim, normalize_regresor=False):
         y = meg.get_data(('sensor', 'time'))
         y = y[:, self.basis.shape[1]:]
-        self._testmeg.append(y/sqrt(y.shape[1]))
-        self._teststim.append(np.dot(covariate_from_stim(stim, self.filter_length, normalize=normalize_regresor),
-                              self.basis)/sqrt(y.shape[1]))
+        self._testmeg.append(y / sqrt(y.shape[1]))
 
-    def setup_es(self, stim):
-        self._es_stim.append(np.dot(covariate_from_stim(stim, self.filter_length, normalize=False),
-                             self.basis))
+        covariates = np.dot(covariate_from_stim(stim, self.filter_length, normalize=normalize_regresor),
+                            self.basis) / sqrt(y.shape[1])
+        if covariates.ndim > 2:
+            self._n_predictor_variables = covariates.shape[0]
+            covariates = covariates.swapaxes(1, 0)
 
-    def eval_model_fit(self):
+        first_dim = covariates.shape[0]
+        self._teststim.append(covariates.reshape(first_dim, -1))
+        # self._teststim.append(np.dot(covariate_from_stim(stim, self.filter_length, normalize=normalize_regresor),
+        #                       self.basis)/sqrt(y.shape[1]))
+
+    def setup_es( self, stim ):
+        covariates = np.dot(covariate_from_stim(stim, self.filter_length, normalize=True),
+                            self.basis)
+        if covariates.ndim > 2:
+            self._n_predictor_variables = covariates.shape[0]
+            covariates = covariates.swapaxes(1, 0)
+
+        first_dim = covariates.shape[0]
+        self._es_stim.append(covariates.reshape(first_dim, -1))
+        # self._es_stim.append(np.dot(covariate_from_stim(stim, self.filter_length, normalize=False),
+        #                      self.basis))
+
+    def eval_model_fit( self ):
         v = 0
         for trial in range(self.n_trials):
             y = self._testmeg[trial] - np.dot(np.dot(self.lead_field, self.theta), self._teststim[trial].T)
             Cb = np.dot(y, y.T)
             v = v + 0.5 * np.trace(linalg.solve(self.Sigma_b[trial], Cb)) \
-                + 2 * np.sum(np.log(np.diag(linalg.cholesky(self.Sigma_b[trial]))))
+                + np.sum(np.log(np.diag(linalg.cholesky(self.Sigma_b[trial]))))
 
         return v
 
-    def setup_from_self(self, obj):
+    def setup_from_self( self, obj ):
         import copy
         self._meg = obj._meg
         self._covariates = obj._covariates
@@ -48,7 +66,7 @@ class DstRFcv(DstRF):
         self._teststim = obj._teststim
         self._es_stim = obj._es_stim
 
-    def get_lead_field(self):
+    def get_lead_field( self ):
         if self.orientation == 'free':
             dims = (self.sensor, self.source, self.space)
             lead_field = NDVar(self.lead_field.reshape(self.lead_field.shape[0], -1, 3), dims=dims)
@@ -61,7 +79,7 @@ class DstRFcv(DstRF):
         return lead_field
 
 
-def compute_ES_metric(models):
+def compute_ES_metric( models ):
     """
     Estimation Stability matric
 
@@ -72,10 +90,8 @@ def compute_ES_metric(models):
     list containing pseudo estimates
     :return: ES_mu
     """
-    Y = np.array ([np.dot(model.lead_field, model.theta).ravel() for model in models])
-    Y_bar = np.mean (Y, axis=0)
+    Y = np.array([np.dot(model.lead_field, model.theta).ravel() for model in models])
+    Y_bar = np.mean(Y, axis=0)
     VarY = linalg.norm(Y - Y_bar, 'fro') ** 2
 
-    return VarY / linalg.norm (Y_bar) ** 2
-
-
+    return VarY / linalg.norm(Y_bar) ** 2
--- dstrf/_basis.py
+++ dstrf/_basis.py
@@ -3,15 +3,15 @@
 import numpy as np
 
 
-def gaussian_basis(nlevel, span):
+def gaussian_basis( nlevel, span ):
     x = span
-    means = np.linspace( x[-1]/nlevel, x[-1]*(1 - 1/nlevel), num = nlevel-1 )
+    means = np.linspace(x[-1] / nlevel, x[-1] * (1 - 1 / nlevel), num=nlevel - 1)
     stds = 8.5
     W = []
 
-    for count in list(range(nlevel-1)):
-        W.append( np.exp( -(x - means[count])**2/(2*stds**2) ) )
+    for count in list(range(nlevel - 1)):
+        W.append(np.exp(-(x - means[count]) ** 2 / (2 * stds ** 2)))
 
     W = np.array(W)
 
-    return W.T/np.max(W)
+    return W.T / np.max(W)
