4407bff88ba1cdb9c2f3d9467bba5f428e2a4f59
Christian Brodbeck <christianmbrodbeck@gmail.com>
2019-02-08 18:06:22 -0500

    RF DstRF:  don't require REG_Data for .get_strf()

--- dstrf/_dstrf.py
+++ dstrf/_dstrf.py
@@ -151,10 +151,7 @@ def dstrf(meg, stim, lead_field, noise, tstart=0, tstop=0.5, nlevels=1,
 
     model = DstRF(lead_field, noise_cov, n_iter=n_iter, n_iterc=n_iterc, n_iterf=n_iterf)
     model.fit(ds, mu, do_crossvalidation, tol, verbose, mus=mus, n_splits=n_splits, n_workers=n_workers)
-
-    trf = model.get_strf(ds)
-
-    return trf, model
+    return model
 
 
 def iter(meg, stim):
