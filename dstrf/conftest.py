de79262edd9d546b89a27a98e00728ebd4269475
proloyd <proloy@umd.edu>
2019-03-06 18:42:11 -0500

    TESTS update the specific prestored values

--- dstrf/conftest.py
+++ dstrf/conftest.py
@@ -3,7 +3,7 @@ import pytest
 # decide on cross-validation test from command line
 def pytest_addoption(parser):
     parser.addoption(
-        "--crossvalidation", action="store", default="True", help="run crossvalidation? True or False"
+        "--crossvalidation", action="store", default="False", help="run crossvalidation? True or False"
     )
 
 
